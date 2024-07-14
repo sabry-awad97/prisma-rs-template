ffdb9dbbfe82a50c62d63f351078cf543f4a5f91
Sabry Awad <dr.sabry97@gmail.com>
2024-07-14 14:19:00 +0300

    ✨ feat(.cargo/config.toml): Add alias for Prisma CLI command ✨ feat(.gitignore): Ignore target and data directories, autogenerated files ✨ feat(Cargo.toml): Add project dependencies and configuration ✨ feat(migrations/20240714111708_/migration.sql): Add SQL migration for User and Post tables ✨ feat(schema.prisma): Define database schema for User and Post models ✨ feat(src/main.rs): Initialize main Rust application file ✨ feat(src/prisma-cli.rs): Add entry point for Prisma CLI command

---
+++ src/main.rs
@@ -0,0 +1,13 @@
+#[allow(warnings, unused)]
+pub mod db;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    env_logger::Builder::from_env(env_logger::Env::default().default_filter_or("info"))
+        .format_timestamp(None)
+        .init();
+
+    log::info!("Starting application...");
+
+    Ok(())
+}
